dc428a94a0fa0bec2672296729dcb823c8cfd920
leehyewon <tnddl2345@gmail.com>
2022-10-25 16:36:53 +0900

    [add] swagger 추가 및 class view 정의

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from articles import views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    # class 로 view에서 선언시 "views.class이름.as_view()" 형식으로 써줘야 함
+    path('', views.ArticleList.as_view(), name='index'),
+    path('<int:article_id>/', views.ArticleDetail.as_view(), name='article_view'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -4,30 +4,35 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
 from articles.models import Article
 from articles.serializers import ArticleSerializer
+from drf_yasg.utils import swagger_auto_schema
+
 
 # Create your views here.
 
+''' 아래 def articleAPI(request) 함수 코드를 class ArticleList(APIView) 안에 넣으므로써 더욱 유용하게 사용할 수 있다.
 
 # 기존의 pure django 에서 사용하던 templates를 사용하지 않고, rest_framework에서 api 브라우저에 직접 들어가서 조작 할 수 있게끔 만들어준다.
 @api_view(['GET', 'POST'])
-def index(request):
+def articleAPI(request):
     if request.method == 'GET':
     # pure django 에서 사용하던 방식
     # return HttpResponse('연결')
     
         articles = Article.objects.all()
         
-        '''
-        아래와 같이 딕셔너리를 하나하나 만들어 주면 힘들기 떄문에 자동화해서 작성한다. (자동화 = 시리얼라이즈)
+        
+        # 아래와 같이 딕셔너리를 하나하나 만들어 주면 힘들기 떄문에 자동화해서 작성한다. (자동화 = 시리얼라이즈)
         article_date = {
             'title':article.title,
             'content':article.content,
             'create_at':article.create_at,
             'update_at':article.update_at,
         }
-        '''
+        
 
         # serializers 선언 방법 / many=True는 article 하나가 아닌 여러개인 list 형태로 가져온다.
         serializer = ArticleSerializer(articles, many=True)
@@ -48,3 +53,74 @@ def index(request):
             print(serializer.errors)
             # 본래 프론트에 어떠한 부분 때문에 에러가 났는지 보여주는것은 좋지않다. just 404error 메세지를 보여 줄 수 있다(주면 좋다).
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+'''
+
+
+# class 장점 : 상속이 가능하다.
+class ArticleList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        if request.method == 'GET':
+            articles = Article.objects.all()
+            serializer = ArticleSerializer(articles, many=True)
+            return Response(serializer.data)
+
+    @swagger_auto_schema(request_body=ArticleSerializer)
+    def post(self, request, format=None):
+        serializer = ArticleSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+''' 마찬가지로 각각의 method를 정의해놓은 def articleDetailAPI를 class ArticleDetail(APIView)로 정의
+@api_view(['GET', 'PUT', 'DELETE'])
+def articleDetailAPI(request, article_id):
+    if request.method == 'GET':
+        # article = Article.objects.get(id=article_id)
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article, data = request.data)    # 데이터를 수정한 데이터로 다시 받아와
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    elif request.method == 'DELETE':
+        article = get_object_or_404(Article, id=article_id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+'''
+
+
+class ArticleDetail(APIView):
+
+    def get(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
+        serializer = ArticleSerializer(article, data = request.data)    # 데이터를 수정한 데이터로 다시 받아와
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, article_id, format=None):
+        article = get_object_or_404(Article, id=article_id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
